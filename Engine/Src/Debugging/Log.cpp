b63e2b7e4dfef20d75110e5fcf83cf6b06d212ca
Canberk Pitirli <canberk_nurcan@hotmail.com>
2023-09-23 23:42:57 +0300

    Logging has been fixed

--- Engine/Src/Debugging/Log.cpp
+++ Engine/Src/Debugging/Log.cpp
@@ -1,17 +1,12 @@
 #include "Log.h"
 
 namespace Messanger
-{
-    /*
-    std::shared_ptr<spdlog::logger> Log::m_Logger;
-    */
-
-
+{ 
     void Log::Initialize()
     {
-        //pdlog::set_pattern("%^[%T] %s-%L: %v%$");
-        //m_Logger = spdlog::stdout_color_mt("Directx");
-    	//m_Logger->set_level(spdlog::level::level_enum::debug);
+        spdlog::set_pattern("%^[%T] %s-%L: %v%$");
+        m_Logger = spdlog::stdout_color_mt("Directx");
+    	m_Logger->set_level(spdlog::level::level_enum::trace);
     }
    
 }
